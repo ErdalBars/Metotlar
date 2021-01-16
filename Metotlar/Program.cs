97bf1feb131a1d9cdfd1618cdc30b62d77ab265d
Cqoew <57765406+Cqoew@users.noreply.github.com>
2021-01-16 23:42:17 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+            urun1.StokAdedi = 19;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.StokAdedi = 65;
+
+            Urun[] urunler = new Urun[] {urun1, urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.StokAdedi);
+                Console.WriteLine("*************");
+            }
+            Console.WriteLine("------METOTLAR------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+        }
+
+    }
+}
+// Metotlar: tekrar tekrar kullanmayı sağlar. (Dont Repeat Yourself-DRY)
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax - yazım şekilleri
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi:" + urun.Adi );
+        }
+
+        public void Ekle2(string urunadi, string aciklama)
+        {
+
+        }
+    }
+}
